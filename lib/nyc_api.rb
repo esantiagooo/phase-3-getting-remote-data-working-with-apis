5c412021753199b91823a8c3b090b1e7995fd6cc
Eric <eric.yanez50@qmail.cuny.edu>
2022-10-19 11:05:01 -0400

    done

--- lib/nyc_api.rb
+++ lib/nyc_api.rb
@@ -11,8 +11,16 @@ class GetPrograms
     response = Net::HTTP.get_response(uri)
     response.body
   end
+  def program_schools
+    programs = JSON.parse(self.get_programs)
+    programs.collect do |program|
+      program['agency']
+    end
+  end 
 
 end
 
-programs = GetPrograms.new.get_programs
-puts programs
+# programs = GetPrograms.new.get_programs
+# puts programs
+programs = GetPrograms.new
+puts programs.program_schools.uniq
